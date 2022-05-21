365661287e147c9f3bbc6e6fa8133c2067a13666
gustavo-mmh <gustavo-mmello@hotmil.com>
2022-05-21 07:40:52 -0300

    Updtade Forms

--- src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
@@ -1,11 +1,11 @@
 package me.dio.academia.digital.entity.form;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
 
 @Data
 @NoArgsConstructor
